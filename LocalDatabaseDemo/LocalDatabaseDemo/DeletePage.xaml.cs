e183ba81907897a8c7730451bdf12cdd0e208672
Jesé Nehemías Alvarado Salvador <jalvarado@univo.edu.sv>
2019-03-28 10:23:34 -0600

    DeletePage codebihide

--- LocalDatabaseDemo/LocalDatabaseDemo/DeletePage.xaml.cs
+++ LocalDatabaseDemo/LocalDatabaseDemo/DeletePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,19 @@ namespace LocalDatabaseDemo
 
         private void BtnFind_Clicked(object sender, EventArgs e)
         {
+            string DBFilePath =
+                Path.Combine(Environment.GetFolderPath
+                (Environment.SpecialFolder.LocalApplicationData),
+                "NewDB.db3");
 
+            Database database = new Database(DBFilePath);
+
+            Category categoryFinded = new Category();
+            int categoryID =int.Parse( CategoryIDEntry.Text);
+            categoryFinded = database.GetCategoryByID(categoryID);
+
+            lblCategoryID.Text = categoryFinded.CategoryID.ToString();
+            lblCategoryName.Text = categoryFinded.CategoryName;
         }
 
         private void BtnDelete_Clicked(object sender, EventArgs e)
